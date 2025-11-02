379e5bd970a11b3025ea754f8d8ed94c6a77e753
Geet Kumar Sahu <geetsahu1852005@gmail.com>
2025-11-02 16:08:55 +0530

    origin setup

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 await connectDB();
 
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(morgan("dev"));
 
